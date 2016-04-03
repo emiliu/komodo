259a7143ba62f8f3bdf31b8ff6f94f0341bd53de
alyao <alyao@ctemc.org>
2016-04-03 04:05:31 -0400

    More Phone Stuff

--- sendstufftophone.py
+++ sendstufftophone.py
@@ -5,17 +5,23 @@ auth_token = "29851e1d4546574268457ff09472f807"
 
 client = TwilioRestClient(account_sid, auth_token)
 
-client.messages.create(to="+18482310390", from_="+12015089231", body="asdflkjasdlfkjasdlkfjaslkdfjalksdfj")
+contactNumber = "+18482166055"
 
 messages = client.messages.list()
 
-while True:
-	input()
+for i in messages:
+	client.messages.delete_instance(i.sid)
 
+client.messages.create(to=contactNumber, from_="+12015089231", body="Bob has completed The Catcher in the Rye. Would you like to send him a quote? Y/N")
+
+while True:
 	messages = client.messages.list()
 
 	for i in messages:
-		print (i.body)
-
-	for i in messages:
-		client.messages.delete_instance(i.sid)
+		if i.body == "Y":
+			client.messages.create(to=contactNumber, from_="+12015089231", body="Message sent.")
+			break;
+		elif i.body == "N":
+			client.messages.create(to=contactNumber, from_="+12015089231", body="That's too bad.")
+		else:
+			a=1
