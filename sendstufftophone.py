941bb4c4d9f479e830228278e2f291d4729c4e90
alyao <alyao@ctemc.org>
2016-04-03 05:46:57 -0400

    More work

--- sendstufftophone.py
+++ sendstufftophone.py
@@ -5,7 +5,8 @@ auth_token = "29851e1d4546574268457ff09472f807"
 
 client = TwilioRestClient(account_sid, auth_token)
 
-contactNumber = "+18482166055"
+contactNumber = "+18482166055" 
+"""+18482166055"""
 targetNumber = "+17323479687"
 
 messages = client.messages.list()
@@ -23,7 +24,7 @@ while True:
 	for i in messages:
 		if i.body == "Y":
 			client.messages.create(to=contactNumber, from_="+12015089231", body="Message sent.")
-			client.messages.create(to=targetNumber, from_="+12015089231", body="Insert Inspirational Quote");
+			client.messages.create(to=targetNumber, from_="+12015089231", body="Insert Inspirational Quote")
 			okleave = 1
 		elif i.body == "N":
 			client.messages.create(to=contactNumber, from_="+12015089231", body="That's too bad.")
